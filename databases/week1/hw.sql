3fbad1d8f2e31f93cd11647fb6f72504214f3b4d
mkabir <ag.kabir@gmail.com>
2023-01-18 18:15:16 +0100

    tried to remove js3 week1 from db week1

--- databases/week1/hw.sql
+++ databases/week1/hw.sql
@@ -5,9 +5,9 @@ SELECT COUNT(id) as 'Number of Tasks' FROM task;
 --2. Find out how many tasks in the task table do not have a valid due date
 SELECT COUNT(id) FROM task WHERE due_date IS NULL;
 --3. Find all the tasks that are marked as done
-SELECT * FROM task WHERE status_id =(SELECT id FROM status WHERE name='Done');
+SELECT * FROM task INNER JOIN status ON task.status_id = status.id AND status.name='Done';
 --4. Find all the tasks that are not marked as done
-SELECT * FROM task WHERE status_id !=(SELECT id FROM status WHERE name='Done');
+SELECT * FROM task INNER JOIN status ON task.status_id = status.id AND status.name!='Done';
 --5. Get all the tasks, sorted with the most recently created first
 SELECT id,title,created FROM task ORDER BY created DESC;
 --6. Get the single most recently created task
@@ -19,5 +19,5 @@ SELECT task.title, status.name as 'text' FROM task INNER JOIN status ON task.sta
 --9. Get the name of each status, along with a count of how many tasks have that status
 SELECT status.name, COUNT(task.id) as 'No. of tasks' FROM status INNER JOIN task ON status.id=task.status_id GROUP BY task.status_id;
 --10. Get the names of all statuses, sorted by the status with most tasks first
-SELECT status.name, COUNT(task.id) as no_of_task FROM status INNER JOIN task ON status.id=task.status_id GROUP BY task.status_id ORDER BY
+SELECT status.name, COUNT(task.id) as no_of_task FROM status INNER JOIN task ON status.id=task.status_id GROUP BY status.name ORDER BY
 no_of_task DESC;
--- javascript/javascript1/week4/fizzbuzz.js
+++
@@ -1,9 +0,0 @@
-function printFizzBuzz(numberLimit) {
-  for (let i = 1; i < numberLimit; i++) {
-    if (i % 3 === 0 && i % 5 != 0) console.log("Fizz ".repeat(3));
-    else if (i % 3 != 0 && i % 5 === 0) console.log("Buzz ".repeat(5));
-    else if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
-    else console.log(i);
-  }
-}
-printFizzBuzz(50);
